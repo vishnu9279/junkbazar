cb1076fab139e1cfedd8e723ad47600a379b39a8
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2023-10-12 20:41:35 +0100

    updating the index file

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-// Connect to the MongoDB database
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/scrap-metal-pickup-app");
-
-// Create the Express.js server
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.get("/", (req, res) => {
+  res.send("Hello, Junk bazzar!");
 });
 
-// Define the API endpoints
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
