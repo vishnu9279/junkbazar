91b576267327ea0617dcdb838d4bf67ce761ceeb
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2023-10-23 20:08:14 +0100

    updated index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
