f755382bc2730d1ca505b9ac7fb096f05da72d45
Davidokih <davidyole023@gmail.com>
2023-10-25 22:31:08 +0100

    update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ process.on("unhandledRejection", (ex) => {
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
