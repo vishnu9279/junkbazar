01220b82799948694d8271a003acaf604bed3009
Davidokih <davidyole023@gmail.com>
2023-11-16 19:50:46 +0100

    Update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,7 +11,9 @@ const {
     userDocuments,
     getSingleUser,
     getAllUser,
-    updateUser
+    updateUser,
+    resendOTP,
+    updatePassword
 } = require("../controllers/userController");
 const upload = require("../utils/multer");
 const auth = require("../utils/auth");
@@ -26,6 +28,8 @@ router.route("/update-user").patch(upload.single("avatar"), auth, updateUser);
 router.route("/forget-password").patch(forgetPassword);
 router.route("/verify-password-otp").patch(verify_reset_password_otp);
 router.route("/reset-password").patch(resetPassword);
+router.route("/resend-otp").patch(resendOTP);
+router.route("/update-password").patch(updatePassword);
 router.route("/upload").patch(auth,
     upload.fields([
         { name: 'avatar' },
