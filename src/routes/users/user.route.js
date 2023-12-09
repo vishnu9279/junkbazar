35ef6881510aa6fffea5e5347167420ebbf1d8bd
ASHISH KUMAR <ashishkumar@ASHISHs-MacBook-Air.local>
2023-12-09 22:22:06 +0530

    code changes

--- src/routes/users/user.route.js
+++ src/routes/users/user.route.js
@@ -31,8 +31,7 @@ router.route("/confirmPickRequest").post(authenticateJwtMiddleware, confirmPickR
 router.route("/addScrap").post(authenticateJwtMiddleware, addScrap);
 router.route("/generateS3UploadSignedUrl").post(authenticateJwtMiddleware, generateS3UploadSignedUrl); // generateS3SignedUrl
 router.route("/addScrapQuantity").post(authenticateJwtMiddleware, addScrapQuantity);
-// router.route("/addPickUpAddress").post(authenticateJwtMiddleware, addPickUpAddress);
-router.route("/addPickUpAddress").post(addPickUpAddress);
+router.route("/addPickUpAddress").post(authenticateJwtMiddleware, addPickUpAddress);
 router.route("/getUserPickupAddress").get(authenticateJwtMiddleware, getUserPickupAddress);
 
 export default router;
