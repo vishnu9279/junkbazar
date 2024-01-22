be26a107387a751de7e4e64b71c83e7e3bbea468
sahimakhtar <sahimakhtar1020@gmail.com>
2024-01-22 12:14:37 +0530

    vendorQuantity

--- src/controllers/admin/scraps/editScrap.controller.js
+++ src/controllers/admin/scraps/editScrap.controller.js
@@ -53,7 +53,7 @@ const editScrap = asyncHandler (async (req, res) => {
         if (fieldValidator(scrapName))
             obj.scrapName = scrapName;
 
-        const resp = await Scrap.findOneAndDelete({
+        const resp = await Scrap.findByIdAndUpdate({
             scrapId
         }, {
             $set: obj
