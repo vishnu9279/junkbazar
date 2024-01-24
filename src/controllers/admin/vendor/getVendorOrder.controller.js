570bf3b0b8b3ff3b600e6532d8a5f4380199f36f
sahimakhtar <sahimakhtar1020@gmail.com>
2024-01-24 22:03:54 +0530

    platform fee

--- src/controllers/admin/vendor/getVendorOrder.controller.js
+++ src/controllers/admin/vendor/getVendorOrder.controller.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import asyncHandler from "../../../utils/asyncHandler.js";
-import userOrder  from "../../../model/users/userOrder.model.js";
+import userOrderModel  from "../../../model/users/userOrder.model.js";
 import fieldValidator from "../../../utils/fieldValidator.js";
 import ApiError from "../../../utils/ApiError.js";
 import {
@@ -17,40 +17,123 @@ const getVendorOrder = asyncHandler(async (req, res) => {
     console.log("getVendorOrder working");
 
     try {
-        const userId = req.decoded.userId;
+        // const userId = req.decoded.userId;
         let limit = req.query.limit;
         let page = req.query.page;
+        let orderStatus = req.query.orderStatus;
+        const filterValue = req.query.key;
         const vendorId = req.query.vendorId;
 
         if (fieldValidator(limit) || isNaN(page)) limit = 10;
-
+    
         if (fieldValidator(page) || isNaN(page)) page = page || 0;
-        
+    
+        orderStatus = orderStatus.split(",").map(el => parseInt(el));
+        console.log("orderStatus", orderStatus);
         const skip = page * limit;
        
-        const orders = await userOrder.aggregate([
-            {
-                $match: {
-                    vendorId
+        const filterObj = {
+            vendorId
+        };
+
+        if (userOrderModel.length > 0){
+            filterObj.orderStatus = {
+                $in: orderStatus
+            };
+        }
+    
+        if (!fieldValidator(filterValue)){
+            filterObj.$or = [
+                {
+                    fullName: new RegExp(filterValue, "i")
+                },
+                {
+                    phoneNumber: new RegExp(filterValue, "i")
+                },
+                {
+                    orderId: new RegExp(filterValue, "i")
                 }
+            ];
+        }
+    
+        console.log("filterObj", filterObj);
+        const orders = await userOrderModel.aggregate([
+            {
+                $match: filterObj
+            },
+            {
+                $unwind: "$items" // Unwind the items array
             },
             {
                 $lookup: {
-                    as: "scrapInfo",
+                    as: "items.scrapInfo",
                     foreignField: "scrapId",
                     from: "scraps",
-                    localField: "scrapId"
+                    localField: "items.scrapId"
                 }
             },
-            
             {
-                $unwind: "$scrapInfo"
+                $unwind: "$items.scrapInfo"
+            },
+            {
+                $group: {
+                    _id: "$_id",
+                    addressInfo: {
+                        $first: "$addressInfo"
+                    },
+                    addToCartId: {
+                        $first: "$addToCartId" 
+                    },
+                    createdAt: {
+                        $first: "$createdAt" 
+                    },
+                    dialCode: {
+                        $first: "$dialCode"
+                    },
+                    enabled: {
+                        $first: "$enabled" 
+                    },
+                    finalAmount: {
+                        $first: "$finalAmount"
+                    },
+                    fullName: {
+                        $first: "$fullName"
+                    },
+                    items: {
+                        $push: "$items" 
+                    },
+                    markupFee: {
+                        $first: "$markupFee"
+                    },
+                    orderId: {
+                        $first: "$orderId"
+                    },
+                    orderStatus: {
+                        $first: "$orderStatus"
+                    },
+                    phoneNumber: {
+                        $first: "$phoneNumber"
+                    },
+                    updatedAt: {
+                        $first: "$updatedAt" 
+                    },
+                    userId: {
+                        $first: "$userId" 
+                    },
+                    userIdF_k: {
+                        $first: "$userIdF_k" 
+                    },
+                    vendorId: {
+                        $first: "$vendorId"
+                    }
+                }
             },
             {
                 $sort: {
                     createdAt: -1  // Sort in descending order based on the createdAt field
                 }
             },
+                
             {
                 $skip: parseInt(skip)  // Add the skip stage
             },
@@ -58,24 +141,37 @@ const getVendorOrder = asyncHandler(async (req, res) => {
                 $limit: parseInt(limit)  // Add the limit stage
             }
         ]);
-
-        for (let index = 0; index < orders.length; index++){
-            const scrapUrl = await generateS3SignedUrl(orders[index].scrapInfo.docPath);
-
-            console.log("orders[index].orderStatus", orders[index].orderStatus);
-
-            orders[index].scrapInfo.docUrl = scrapUrl;
-        }
-            
-        const totalScrapCount = await userOrder.countDocuments({
-            vendorId: userId
-        });
     
+        // console.log("orders", orders);
+        for (let index = 0; index < orders.length; index++){
+            console.log("inside if condtionvendor ", orders[index].vendorId);
+    
+            orders[index].items.map(async(el) => {
+                const url = await generateS3SignedUrl(el.scrapInfo.docPath);
+    
+                el.scrapInfo.docUrl = url;
+    
+                return el;
+            });
+        }
+                
+        const totalScrapCount = await userOrderModel.aggregate([
+            {
+                $match: {
+                    orderStatus: {
+                        $in: orderStatus
+                    }
+                }
+            }
+        ]);
+    
+        console.log("totalScrapCount", totalScrapCount.length);
+       
         const finalObj = {
             orders: orders,
-            totalScrapCount
+            totalScrapCount: totalScrapCount.length
         };
-
+    
         return res.status(statusCodeObject.HTTP_STATUS_OK).json(
             new ApiResponse(
                 statusCodeObject.HTTP_STATUS_OK,
@@ -87,10 +183,10 @@ const getVendorOrder = asyncHandler(async (req, res) => {
     }
     catch (error) {
         console.error("Error on getting scrap", error.message);
-
+    
         if (error instanceof ApiError) {
             console.log("Api Error instance");
-
+    
             // Handle ApiError instances with dynamic status code and message
             return res.status(error.statusCode).json({
                 error: error || CommonMessage.SOMETHING_WENT_WRONG
@@ -99,7 +195,7 @@ const getVendorOrder = asyncHandler(async (req, res) => {
         else {
             // Handle other types of errors
             console.error("Error in getVendorOrder:", error);
-
+    
             return res.status(statusCodeObject.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
                 error: CommonMessage.SOMETHING_WENT_WRONG
             });
