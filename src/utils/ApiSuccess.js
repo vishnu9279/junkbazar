ba7506c504454956ac5b3874938ecf7ec891a1dc
sahimakhtar <sahimakhtar1020@gmail.com>
2024-02-01 19:43:01 +0530

    address

--- src/utils/ApiSuccess.js
+++ src/utils/ApiSuccess.js
@@ -3,14 +3,13 @@
 import {
     basicConfigurationObject
 } from "../utils/constants.js";
-import fieldValidator from "../utils/fieldValidator.js";
-
+console.log(typeof basicConfigurationObject.JSON_STRINGIFY);
 class ApiResponse {
     constructor(statusCode, successCode, data, message = "Success") {
         console.log("basicConfigurationObject.JSON_STRINGIFY", basicConfigurationObject.JSON_STRINGIFY);
         this.statusCode = statusCode;
         this.successCode = successCode;
-        this.data = (!fieldValidator(basicConfigurationObject.JSON_STRINGIFY)) ? data : JSON.stringify(data);
+        this.data = (basicConfigurationObject.JSON_STRINGIFY === "true") ? data : JSON.stringify(data);
         // this.data =  JSON.stringify(data);
         this.message = message;
         this.success = statusCode < 400;
