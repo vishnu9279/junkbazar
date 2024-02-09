943c87380a49fb642cb7e37263c57e32090621bf
sahimakhtar <sahimakhtar1020@gmail.com>
2024-02-09 19:22:24 +0530

    user model

--- src/utils/ApiSuccess.js
+++ src/utils/ApiSuccess.js
@@ -3,10 +3,9 @@
 import {
     basicConfigurationObject
 } from "../utils/constants.js";
-console.log(typeof basicConfigurationObject.JSON_STRINGIFY);
+console.log("basicConfigurationObject.JSON_STRINGIFY", typeof basicConfigurationObject.JSON_STRINGIFY,   basicConfigurationObject.JSON_STRINGIFY);
 class ApiResponse {
     constructor(statusCode, successCode, data, message = "Success") {
-        console.log("basicConfigurationObject.JSON_STRINGIFY", basicConfigurationObject.JSON_STRINGIFY);
         this.statusCode = statusCode;
         this.successCode = successCode;
         this.data = (basicConfigurationObject.JSON_STRINGIFY === "true") ? data : JSON.stringify(data);
